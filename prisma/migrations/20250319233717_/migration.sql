1f6a82d6d91b331f25ae7b67eb8245addf730b0e
Alex <moyasimarro@gmail.com>
2025-03-20 00:44:22 +0100

    mas cambios

---
+++ prisma/migrations/20250319233717_/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `password_reset_tokens` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `password_reset_tokens_token_key`(`token`),
+    INDEX `password_reset_token_user_id_fkey`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `password_reset_tokens` ADD CONSTRAINT `password_reset_tokens_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
