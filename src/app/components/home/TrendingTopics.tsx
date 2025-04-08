d96837a7b4db47d6cc5ea97aa4a5a4800dc296d8
Alex <moyasimarro@gmail.com>
2025-04-08 17:09:50 +0200

    PRUEBA ANIMACION

--- src/app/components/home/TrendingTopics.tsx
+++ src/app/components/home/TrendingTopics.tsx
@@ -45,9 +45,9 @@ export function TrendingTopics() {
         {trendingTopics.map((topic, index) => (
           <motion.div
             key={topic.name}
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: index * 0.1 }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.1, duration: 0.3 }}
             className="space-y-2 group cursor-pointer"
           >
             <div className="flex items-center justify-between">
